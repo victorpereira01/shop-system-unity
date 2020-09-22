7e4a66e4b96e3eec242af854c3f4b0b7e158d67a
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-22 18:53:22 -0300

    Shop grid created and populating with prefabs

---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    
+    public GameObject[] planets;
+
+    private void Start() {}
+
+    
+}
